d092b101465c24506efbcf55affa51c25d2f3bea
R3ygoski <hckranonymous2@hotmail.com>
2025-04-16 06:58:15 -0300

    chore: setup initial project

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: "variable",
+  style: "normal",
+  variable: "--font-inter",
+});
+
+export const metadata: Metadata = {
+  title: "Portfólio | Bernardo",
+  description: "Portfólio do Bernardo",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} h-dvh w-full`}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <main className="w-full h-[calc(100vh-3rem)] bg-white dark:bg-black"></main>
+  );
+}
