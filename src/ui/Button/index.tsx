4c6b79d78c2dbbacb58c24702e89e42dd80ee0d0
R3ygoski <hckranonymous2@hotmail.com>
2025-04-18 20:06:23 -0300

    style: make button not wrap when category is collapsing

--- src/ui/Button/index.tsx
+++ src/ui/Button/index.tsx
@@ -8,7 +8,7 @@ interface IButtonProps {
 export default function Button({ label, action }: IButtonProps) {
   return (
     <button
-      className="py-2 px-4 border rounded-lg ease-in-out duration-300 cursor-pointer border-black/25 text-shark-900 dark:border-white/25 dark:text-shark-100 hover:bg-black/10 dark:hover:bg-white/10"
+      className="min-w-fit py-2 px-4 border rounded-lg ease-in-out duration-300 cursor-pointer border-black/25 text-shark-900 dark:border-white/25 dark:text-shark-100 hover:bg-black/10 dark:hover:bg-white/10"
       onClick={() => action()}
     >
       {label}
