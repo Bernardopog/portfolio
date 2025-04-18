81e9f900544ef10f1ff48831d8a21b5411f276f6
R3ygoski <hckranonymous2@hotmail.com>
2025-04-18 19:10:09 -0300

    feat: create button component

---
+++ src/ui/Button/index.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+interface IButtonProps {
+  label: string;
+  action: () => void;
+}
+
+export default function Button({ label, action }: IButtonProps) {
+  return (
+    <button
+      className="py-2 px-4 border rounded-lg ease-in-out duration-300 cursor-pointer border-black/25 text-shark-900 dark:border-white/25 dark:text-shark-100 hover:bg-black/10 dark:hover:bg-white/10"
+      onClick={() => action()}
+    >
+      {label}
+    </button>
+  );
+}
