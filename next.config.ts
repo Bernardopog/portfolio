74bae67dca2a892ced1fe8c283a22da793d07946
Bernardo Poggioni <hckranonymous2@hotmail.com>
2025-10-07 04:34:11 -0300

    chore: configure next image to accept webp

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    formats: ["image/webp"],
+    qualities: [75, 50, 25],
+  },
   devIndicators: false,
 };
 
