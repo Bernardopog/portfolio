5da4435a0de47df0f5da07fcb7ca94bad171ec6b
Bernardo Poggioni <hckranonymous2@hotmail.com>
2025-11-25 22:20:41 -0300

    feat(ui/button): add title

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -9,6 +9,7 @@ interface IButtonProps {
   className?: string;
   ariaLabel?: string;
   disabled?: boolean;
+  title?: string;
 }
 
 export default function Button({
@@ -18,10 +19,12 @@ export default function Button({
   className,
   ariaLabel,
   disabled,
+  title,
 }: IButtonProps) {
   return (
     <button
       {...(ariaLabel && { 'aria-label': ariaLabel })}
+      {...((title || ariaLabel) && { title: title || ariaLabel })}
       className={`flex items-center gap-2 cursor-pointer disabled:cursor-not-allowed ${
         className ?? 'btn-default btn-default-color'
       }`}
