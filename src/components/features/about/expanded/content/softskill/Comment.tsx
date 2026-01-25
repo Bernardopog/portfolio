528301aa7498f05d988c699517f50ae33854b50f
Bernardo Poggioni <hckranonymous2@hotmail.com>
2026-01-25 12:55:19 -0300

    style(about/softskill): change evidence bullets color

--- src/components/features/about/expanded/content/softskill/Comment.tsx
+++ src/components/features/about/expanded/content/softskill/Comment.tsx
@@ -1,6 +1,25 @@
-import type { IComments } from '@/data/content/about/softskillComments';
+import type {
+  IComments,
+  SoftskillType,
+} from '@/data/content/about/softskillComments';
 
 export default function Comment({ title, href, softskills }: IComments) {
+  const sofskillColor = (softskill: SoftskillType) => {
+    const colorMap = new Map<
+      SoftskillType,
+      { bg: string; circle: string; text?: string }
+    >();
+
+    colorMap.set('communication', { bg: 'bg-blue-200', circle: 'bg-blue-400' });
+    colorMap.set('empathy', { bg: 'bg-red-200', circle: 'bg-red-400' });
+    colorMap.set('colaboration', {
+      bg: 'bg-emerald-200',
+      circle: 'bg-emerald-400',
+    });
+
+    return colorMap.get(softskill);
+  };
+
   return (
     <li className='shadow-lg rounded-lg overflow-hidden'>
       <a
@@ -8,16 +27,18 @@ export default function Comment({ title, href, softskills }: IComments) {
         target='_blank'
         className='flex flex-col h-28 border p-2 rounded-lg bg-gradient-to-b from-white to-shark-100 border-black/15 dark:from-black dark:to-shark-950 dark:border-white/15'
       >
-        <p className='flex-1 text-shark-950 dark:text-shark-50'>{title} #WIP</p>
+        <p className='flex-1 text-shark-950 dark:text-shark-50'>{title}</p>
         <ul className='flex px-2 gap-2 overflow-x-auto scrollbar-thin scrollbar-thumb-shark-950 scrollbar-track-transparent dark:scrollbar-thumb-shark-50'>
           {softskills.map((softskill) => {
             return (
               <li
                 key={softskill}
-                className='flex items-center gap-2 p-0.5 px-1 rounded-lg text-xs capitalize'
+                className={`flex items-center gap-2 p-0.5 px-1 rounded-lg text-xs capitalize ${sofskillColor(softskill)?.bg}`}
               >
-                <span className='inline-block w-2 h-2 rounded-full bg-shark-400 dark:bg-shark-500'></span>
-                <p className='text-shark-950 dark:text-shark-50'>{softskill}</p>
+                <span
+                  className={`inline-block w-2 h-2 rounded-full ${sofskillColor(softskill)?.circle}`}
+                ></span>
+                <p className='text-shark-950'>{softskill}</p>
               </li>
             );
           })}
