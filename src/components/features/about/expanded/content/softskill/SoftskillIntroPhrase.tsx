156562850824dc5cda26310eb7c28e602ea87e64
Bernardo Poggioni <hckranonymous2@hotmail.com>
2026-02-11 23:26:13 -0300

    style(about/softskill): add animation to intro phrase

--- src/components/features/about/expanded/content/softskill/SoftskillIntroPhrase.tsx
+++ src/components/features/about/expanded/content/softskill/SoftskillIntroPhrase.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { useTranslations } from 'next-intl';
 import { useEffect, useState } from 'react';
+import { MdFormatQuote } from 'react-icons/md';
 
 export default function SoftskillIntroPhrase() {
   const t = useTranslations('AboutMe.Expanded.Softskill');
@@ -16,9 +17,12 @@ export default function SoftskillIntroPhrase() {
   return (
     <>
       {selectedPhrase && (
-        <p className='text-center text-lg  font-bold text-shark-900 dark:text-shark-100 md:text-xl'>
-          {t(`Phrases.${selectedPhrase}`)}
-        </p>
+        <div className='relative animate-fade-in text-shark-900 dark:text-shark-100'>
+          <MdFormatQuote className='absolute -top-4 -right-4 text-2xl' />
+          <p className='text-center text-lg  font-bold md:text-xl'>
+            {t(`Phrases.${selectedPhrase}`)}
+          </p>
+        </div>
       )}
     </>
   );
