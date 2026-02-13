63bc67a9282ea21b2250b3770775cd8f92b9ffb7
Bernardo Poggioni <hckranonymous2@hotmail.com>
2026-02-13 13:44:16 -0300

    content(about/softskill): add Frontend Mentor comments

--- src/data/content/about/softskillComments.ts
+++ src/data/content/about/softskillComments.ts
@@ -1,35 +1,50 @@
-export type SoftskillType = 'empathy' | 'communication' | 'colaboration';
+export type SoftskillType =
+  | 'Empathy'
+  | 'Communication'
+  | 'Didactics'
+  | 'Mentoring'
+  | 'Accessbility'
+  | 'CleanCode'
+  | 'Diplomacy'
+  | 'CriticalThinking'
+  | 'Performance';
 
 export interface IComments {
   title: string;
   href: string;
-  softskills: SoftskillType[];
+  softskills: SoftskillType[]; //?Tags
+  description: string;
 }
 
 export const softskillComments: IComments[] = [
   {
-    title: 'Comentário 1',
-    href: '#',
-    softskills: ['colaboration', 'communication', 'empathy'],
+    title: 'Comment1.Title',
+    href: 'https://www.frontendmentor.io/solutions/blog-preview-card-responsivo---html-semntico-flexbox-e-hover-effects-drDxt4yGt5',
+    softskills: ['Mentoring', 'CleanCode'],
+    description: 'Comment1.Description',
   },
   {
-    title: 'Comentário 2',
-    href: '#',
-    softskills: [],
+    title: 'Comment2.Title',
+    href: 'https://www.frontendmentor.io/solutions/testimonials-grid-section-using-tailwind-ECd1QVIBjR',
+    softskills: ['Accessbility', 'Didactics'],
+    description: 'Comment2.Description',
   },
   {
-    title: 'Comentário 3',
-    href: '#',
-    softskills: [],
+    title: 'Comment3.Title',
+    href: 'https://www.frontendmentor.io/solutions/responsive-page-to-get-advices-qjTpuLf3yP',
+    softskills: ['CriticalThinking', 'CleanCode', 'Empathy'],
+    description: 'Comment3.Description',
   },
   {
-    title: 'Comentário 4',
-    href: '#',
-    softskills: [],
+    title: 'Comment4.Title',
+    href: 'https://www.frontendmentor.io/solutions/testimonials-grid-section-using-tailwind-ECd1QVIBjR',
+    softskills: ['Diplomacy', 'CriticalThinking', 'Performance'],
+    description: 'Comment4.Description',
   },
   {
-    title: 'Comentário 5',
-    href: '#',
-    softskills: [],
+    title: 'Comment5.Title',
+    href: 'https://www.frontendmentor.io/solutions/ok-maybe-grids-are-cool-4I98hQpH6f',
+    softskills: ['Mentoring', 'Communication', 'Empathy'],
+    description: 'Comment5.Description',
   },
 ];
