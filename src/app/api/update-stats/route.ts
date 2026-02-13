baf31e311a2c136b6e246c97f3ae653eff027a41
Bernardo Poggioni <hckranonymous2@hotmail.com>
2026-02-13 11:54:35 -0300

    refactor(scrapper): improve robustness with error handling and timeouts

--- src/app/api/update-stats/route.ts
+++ src/app/api/update-stats/route.ts
@@ -1,68 +1,80 @@
-import { kv } from "@vercel/kv";
-import { NextResponse } from "next/server";
-import { chromium } from "playwright";
+import { kv } from '@vercel/kv';
+import { NextResponse } from 'next/server';
+import { chromium } from 'playwright';
 
 export async function GET() {
-  const browser = await chromium.launch({
-    headless: true,
-    args: ["--no-sandbox"],
-  });
+  try {
+    const browser = await chromium.launch({
+      headless: true,
+      args: ['--no-sandbox'],
+    });
 
-  const storageState = JSON.parse(process.env.AUTH_JSON || "{}");
+    const storageState = JSON.parse(process.env.AUTH_JSON || '{}');
 
-  const context = await browser.newContext({ storageState });
-  const page = await context.newPage();
+    const context = await browser.newContext({ storageState });
+    const page = await context.newPage();
 
-  await page.goto("https://www.frontendmentor.io/home");
+    await page.goto('https://www.frontendmentor.io/home', {
+      waitUntil: 'load',
+      timeout: 20000,
+    });
 
-  //? Verify if it's logged or not
-  const loggedOutText = page.locator("h1");
+    //? Verify if it's logged or not
+    const loggedOutText = page.locator('h1');
 
-  if ((await loggedOutText.innerText()) === "Oops! You’re not logged in") {
-    return NextResponse.json({ ok: false, message: "Not logged in" });
-  }
-
-  const liMentorScore = page.locator('li:has-text("Mentor Score")');
-  const liComments = page.locator('li:has-text("Comments")');
-
-  const mentorScore = await liMentorScore.locator("span").nth(1).innerText();
-  const comments = await liComments.locator("span").nth(1).innerText();
-
-  const liHelpfulComments = page.locator('li:has-text("Helpful comments")');
-  const helpfulComments = await liHelpfulComments
-    .locator("span")
-    .nth(1)
-    .innerText();
-
-  await page.goto("https://www.frontendmentor.io/wall-of-fame?tab=all");
-  await page.waitForSelector("text=Bernardopog");
-
-  const rankList = await page
-    .locator("main > div:nth-of-type(2) div ul li")
-    .all();
-
-  let position = -1;
-  for (let i = 0; i < rankList.length; i++) {
-    const text = await rankList[i].innerText();
-    if (
-      text.toLowerCase().includes("bernardopog") ||
-      text.toLowerCase().includes("bernardo poggioni")
-    ) {
-      position = i + 1;
-      break;
+    if ((await loggedOutText.innerText()) === 'Oops! You’re not logged in') {
+      return NextResponse.json({ ok: false, message: 'Not logged in' });
     }
+
+    const liMentorScore = page.locator('li:has-text("Mentor Score")');
+    const liComments = page.locator('li:has-text("Comments")');
+
+    const mentorScore = await liMentorScore.locator('span').nth(1).innerText();
+    const comments = await liComments.locator('span').nth(1).innerText();
+
+    const liHelpfulComments = page.locator('li:has-text("Helpful comments")');
+    const helpfulComments = await liHelpfulComments
+      .locator('span')
+      .nth(1)
+      .innerText();
+
+    await page.goto('https://www.frontendmentor.io/wall-of-fame?tab=all');
+    await page.waitForSelector('text=Bernardopog');
+
+    const rankList = await page
+      .locator('main > div:nth-of-type(2) div ul li')
+      .all();
+
+    let position = -1;
+    for (let i = 0; i < rankList.length; i++) {
+      const text = await rankList[i].innerText();
+      if (
+        text.toLowerCase().includes('bernardopog') ||
+        text.toLowerCase().includes('bernardo poggioni')
+      ) {
+        position = i + 1;
+        break;
+      }
+    }
+
+    const convertedValues = (value: string) => parseInt(value, 10);
+
+    await kv.set('frontendmentor', {
+      mentorScore: convertedValues(
+        mentorScore.replaceAll(',', '').replaceAll('.', ''),
+      ),
+      comments: convertedValues(comments),
+      helpfulComments: convertedValues(helpfulComments),
+      wallOfFame: position,
+    });
+
+    await browser.close();
+
+    return NextResponse.json({
+      ok: true,
+      message: 'Data updated successfully',
+    });
+  } catch {
+    return NextResponse.json({ ok: false, message: 'Unexpected error' });
   }
-
-  const convertedValues = (value: string) => parseInt(value, 10);
-
-  await kv.set("frontendmentor", {
-    mentorScore: convertedValues(
-      mentorScore.replaceAll(",", "").replaceAll(".", ""),
-    ),
-    comments: convertedValues(comments),
-    helpfulComments: convertedValues(helpfulComments),
-    wallOfFame: position,
-  });
-
-  return NextResponse.json({ ok: true, message: "Data updated successfully" });
 }
